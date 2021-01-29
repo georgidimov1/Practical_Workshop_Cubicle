0f8c63b6c04c5f2c2166fe090170cc17b41297cf
Georgi Dimov <g.dimov@gmail.com>
2021-01-29 07:35:40 +0200

    view engine
    add handlebars

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require ('express');
+const handlebars = require ('express-handlebars')
 const config = require ('./config/config');
 const app = express();
 
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
 app.get('/', (req, res)=>{
-    res.send('hi')
+    res.render('home', {layout:false});
      })
 app.listen(config.PORT , ()=>{console.log(`Listening ...on port: ${config.PORT}`)})
