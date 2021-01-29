5161856af7e94f0ddc7f7e759c99605290a23370
Georgi Dimov <g.dimov@gmail.com>
2021-01-29 07:19:28 +0200

    set environment variable
    implementation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const express = require ('express');
+const config = require ('./config/config');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('hi')
+     })
+app.listen(config.PORT , ()=>{console.log(`Listening ...on port: ${config.PORT}`)})
