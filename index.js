88bd88ae2aadc46c4ee161cab68271bc3d7edbf2
Georgi Dimov <g.dimov@gmail.com>
2021-01-30 17:50:36 +0200

    rendering all views
    layout: false

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ app.engine('hbs', handlebars({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs')
+app.use(express.static('static') )
+app.use(express.urlencoded());
+app.use(express.json());
+
 app.get('/', (req, res)=>{
     res.render('home', {layout:false});
      })
+app.route('/create')
+.get( (req, res)=>{
+    res.render('create', {layout:false})
+})
+.post( (req, res)=>{
+    res.send(req.body.name);
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about',  {layout:false})
+})
+app.all
 app.listen(config.PORT , ()=>{console.log(`Listening ...on port: ${config.PORT}`)})
