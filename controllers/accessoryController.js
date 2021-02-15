97bbcca37c7eee858bd66f3794dacd11aebf5529
Georgi Dimov <g.dimov@gmail.com>
2021-02-15 06:44:49 +0200

    product Service

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -2,13 +2,14 @@ const {Router} = require ('express');
 const router = Router();
 const bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const accessoryServices = require('../services/productService');
+const accessoryServices = require('../services/accessoryService.js');
 
-router.get('/create', (req, res)=>{
+router.get('/', (req, res)=>{
     res.render('createAccessory', {title: 'Create Accessory'});
      })
-router.post('/create', urlencodedParser, (req, res)=>{
+router.post('/create',urlencodedParser, (req, res)=>{
     // validate inputs
+    //console.log(req.body)
     accessoryServices.create(req.body)
     .then(()=>{res.redirect('/')})
     .catch(()=> res.status(500).end()) 
